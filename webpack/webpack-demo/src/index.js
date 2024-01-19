098126fda57f3669c90922b630af0ce86502064a
thinkasany <480968828@qq.com>
2024-01-19 10:16:12 +0800

    feat: webpack demo

---
+++ webpack/webpack-demo/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+import "../assets/text.txt";
+import("./log").then(log => {
+  console.log(999);
+});
+fetch("http://localhost:7000/api/");
+
+React.createElement("div");
+console.log("111");
---
+++ webpack/webpack-demo/webpack.dev.js
@@ -0,0 +1,65 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require("path");
+const config = {
+  entry: {
+    index: {
+      import: "./src/index.js"
+      //   dependOn: ["react-vendor", "test"]
+    }
+    // "react-vendor": {
+    //   import: ["react"]
+    //   //   import: ["react", "redux"]
+    // },
+    // test: {
+    //   import: "./src/test.js",
+    //   filename: "app.js"
+    // }
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    // publicPath: "https://a.b.c/assets/"
+    filename: "test_demo.js",
+    chunkFilename: "asset_[id].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"] // MiniCssExtractPlugin是将css抽出文件的，会和style-loader冲突
+        // use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [new CssMinizerPlugin()]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html")
+    })
+    // new MiniCssExtractPlugin({
+    //   filename: "[name].css",
+    //   chunkFilename: "[name].css"
+    // })
+  ],
+  devtool: "source-map",
+  devServer: {
+    client: {
+      overlay: false
+    },
+    compress: true,
+    hot: "only", // 设置编译出错，改正不会刷新浏览器
+    open: false // 是否打开新的浏览器tab
+  }
+};
+module.exports = (env, argv) => {
+  console.log(env, argv);
+  if (argv.mode === "development") {
+    config.output.filename = "dev_demo.js";
+  } else if (argv.mode === "production") {
+    config.output.filename = "prod_demo.js";
+  }
+  return config;
+};
